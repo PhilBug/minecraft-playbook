dcae876a4bcd87b98a429d05d1928481d17731e2
Filip Bugaj <f.bugaj@merixstudio.com>
2022-03-20 03:10:50 +0100

    increase weather-clear frequency

--- data/minecraft-weather-clear.sh
+++ data/minecraft-weather-clear.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 echo 'weather rain' | socat EXEC:"/usr/bin/docker attach minecraft_server",pty STDIN
-sleep 2
+sleep 1
 echo 'weather clear 24000' | socat EXEC:"/usr/bin/docker attach minecraft_server",pty STDIN
